4fda036b687d00c4ccb1fcac025a193ee3d88e09
Siddarth Singotam <44916541+siddarthsingotam@users.noreply.github.com>
2022-11-11 09:51:11 +0530

    Delete Exercise_4 directory

--- Exercise_4/4.1.py
+++
@@ -1,4 +0,0 @@
-x = 0
-while x < 999 :
-    print (x+3)
-    x=x+3
--- Exercise_4/4.2.py
+++
@@ -1,6 +0,0 @@
-inch = float(input("Enter in inches :"))
-while inch >= 0:
-    print("The cm is :",inch*2.54)
-    break
-else:
-    print("Invalid inches")
