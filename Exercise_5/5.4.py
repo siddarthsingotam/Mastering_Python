2a4d0a78d39daa64ac851a16f667705745bf7cde
Siddarth Singotam <44916541+siddarthsingotam@users.noreply.github.com>
2022-11-11 09:51:31 +0530

    Delete Exercise_5 directory

--- Exercise_5/5.4.py
+++
@@ -1,9 +0,0 @@
-cities = []
-
-for n in range(0, 5):
-    name = input(f"Enter the city {n+1}: ")
-    cities.append(name)
-
-print(f"\nThe order of cities are: \n ")
-for m in cities:
-    print(f"City {cities.index(m)+1} is: {m.capitalize()}")
